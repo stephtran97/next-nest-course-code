18d065dfc9680fccd31148b6cc43c009641b7c64
ThinhTD <tranducthinh.97@gmail.com>
2024-09-16 01:17:57 +0700

    Init react module code

---
+++ reactjs/exercise/ex2/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { InformationCircleIcon as Icon } from '@heroicons/react/20/solid';
+import './style.css';
+
+// Use curly braces to display banner data
+export default function Banner() {
+  const bannerData = {
+    icon: (
+      <Icon
+        className='banner-icon'
+        aria-hidden='true'
+      />
+    ),
+    version: 5,
+    href: '/details'
+  };
+
+  return (
+    <div className='banner-wrapper'>
+      <div className='flex'>
+        <div className='flex-shrink-0'>{bannerData.icon}</div>
+        <div className='banner-content'>
+          <p className='banner-text'>
+            A new software update is available. See what’s new in version{' '}
+            {bannerData.version}
+          </p>
+          <p className='banner-details'>
+            <a
+              className='banner-details-link'
+              href={bannerData.href}
+            >
+              Details
+              <span aria-hidden='true'> &rarr;</span>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ reactjs/exercise/exercises-1/ex1.md
@@ -0,0 +1,18 @@
+# Ex1: Convert HTML to JSX
+
+```ts
+export default function Pricing() {
+  return (
+    <>
+      <h2 className='title'>Simple no-tricks pricing</h2>
+      <hr />
+      <a
+        href='/buy'
+        className='button'
+      >
+        Buy Now <span aria-hidden='true'>→</span>
+      </a>
+    </>
+  );
+}
+```
